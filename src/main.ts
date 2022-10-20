662118524d9b374800c0db17c6cc6852ee8fb280
Helltis <59091609+Helltis@users.noreply.github.com>
2022-10-21 00:10:00 +0300

    disabled CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
