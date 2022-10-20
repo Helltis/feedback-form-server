0eb8e6b6299db919bb4e697e9ce4a7575b4e4af2
Helltis <59091609+Helltis@users.noreply.github.com>
2022-10-21 01:30:47 +0300

    entity

--- src/feedback/models/post.entity.ts
+++ src/feedback/models/post.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity(process.env.PGDATABASE)
+@Entity()
 export class FeedbackPostEntity {
   @PrimaryGeneratedColumn()
   id: number;
